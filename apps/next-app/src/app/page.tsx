0e999a3f7c660c3183aa949ac987a0c2f0a028d9
2005LIKHITH <likhithcvsrl@gmail.com>
2025-01-19 23:40:39 +0530

    db package added

--- apps/next-app/src/app/page.tsx
+++ apps/next-app/src/app/page.tsx
@@ -1,8 +1,9 @@
 import {Input, Switch} from "@repo/ui"
 import { Button } from "@repo/ui";
+import db from "@repo/db/client"
 const Home: React.FC = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen w-full bg-black">
+    <div className="flex items-center justify-center min-h-screen w-full">
       <div className="text-yellow-200  text-5xl">This is a TurboRepo 🚀</div>
       <Button variant={"secondary"} className="ml-2">Click here to Launch</Button>
     </div>
